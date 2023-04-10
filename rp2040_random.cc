a3d94c77fedd5f0640c9c215e4745e37d6a7aeb1
Jeffrey Lim <jeff@lim.au>
2023-04-10 19:29:16 +0800

    Separate hardware random from stdlib random

--- rp2040_random.cc
+++ rp2040_random.cc
@@ -6,7 +6,7 @@
 
 volatile uint32_t *const randomBit = (uint32_t *)0x4006001c;
 
-uint32_t Random::GenerateUint32() {
+uint32_t Random::GenerateHardwareUint32() {
   uint32_t result = 0;
   for (int i = 0; i < 32; ++i) {
     result = (result << 1) | (*randomBit & 1);
