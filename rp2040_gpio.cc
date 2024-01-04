482e306a42e1a1eacd6b70b3ad457c7a98ce0686
Jeffrey Lim <jeff@lim.au>
2024-01-05 02:11:04 +0800

    Update with SetPinDutyCycle implementation

--- rp2040_gpio.cc
+++ rp2040_gpio.cc
@@ -2,6 +2,7 @@
 
 #include "javelin/hal/gpio.h"
 #include <hardware/gpio.h>
+#include <hardware/pwm.h>
 
 //---------------------------------------------------------------------------
 
@@ -26,6 +27,14 @@ void Gpio::SetPin(int pin, bool value) {
   gpio_init(pin);
   gpio_set_dir(pin, true);
   gpio_put(pin, value);
+}
+
+void Gpio::SetPinDutyCycle(int pin, int dutyCycle) {
+  gpio_set_function(pin, GPIO_FUNC_PWM);
+  uint32_t slice_num = pwm_gpio_to_slice_num(pin);
+  pwm_set_wrap(slice_num, 99);
+  pwm_set_enabled(slice_num, true);
+  pwm_set_gpio_level(pin, dutyCycle);
 }
 
 //---------------------------------------------------------------------------
