4896dd4fc2bb4bb884633ebd706dbe677d307c35
Jeffrey Lim <jeff@lim.au>
2024-07-26 01:23:36 +0800

    Sync with latest internal repository

--- pico_bindings.cc
+++ pico_bindings.cc
@@ -544,6 +544,9 @@ void InitJavelinSlave() { InitCommonCommands(); }
 void InitJavelinMaster() {
   const StenoConfigBlock *config = STENO_CONFIG_BLOCK_ADDRESS;
 
+  ScriptManager::GetInstance().SetAllowButtonStateUpdates(
+      config->allowButtonStateUpdates);
+
 #if JAVELIN_USE_EMBEDDED_STENO
   HidKeyboardReportBuilder::instance.SetCompatibilityMode(
       config->hidCompatibilityMode);
